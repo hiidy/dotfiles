e57bcbac9dc4225d6c0344b9f48ef693284d2e41
Woong <hiidy115@gmail.com>
2025-12-19 13:07:24 +0900

    fix init.lua error

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -2,6 +2,7 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
--- 저장/종료 단축키 (VS Code 스타일)
-vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
-vim.keymap.set({ "i", "x", "n", "s" }, "<C-q>", "<cmd>q<cr>", { desc = "Quit File" })
+vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
+vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-q>', '<cmd>q<cr>', { desc = 'Quit File' })
+
+return {}
