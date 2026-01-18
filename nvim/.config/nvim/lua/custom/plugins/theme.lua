44d69f8cb51ac59d8226d867646f722f1f134447
Woong <hiidy115@gmail.com>
2026-01-18 20:48:48 +0900

    feat: update Neovim theme to TokyoNight and add Tmux theme configuration

--- nvim/.config/nvim/lua/custom/plugins/theme.lua
+++ nvim/.config/nvim/lua/custom/plugins/theme.lua
@@ -1,20 +1,17 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
+  'folke/tokyonight.nvim',
+  lazy = false,
   priority = 1000,
-  config = function()
-    require('catppuccin').setup {
-      flavour = 'mocha', -- latte, frappe, macchiato, mocha
-      transparent_background = true, -- Ghostty 투명도 반영
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        treesitter = true,
-        mason = true,
-        telescope = { enabled = true },
-      },
-    }
-
-    vim.cmd.colorscheme 'catppuccin'
+  opts = {
+    style = 'storm', -- 'storm', 'moon', 'night', 'day' 중 선택 가능
+    transparent = true, -- Ghostty 투명도 반영
+    styles = {
+      sidebars = 'transparent', -- 사이드바 투명 설정
+      floats = 'transparent',   -- 플로팅 윈도우 투명 설정
+    },
+  },
+  config = function(_, opts)
+    require('tokyonight').setup(opts)
+    vim.cmd.colorscheme 'tokyonight'
   end,
 }
